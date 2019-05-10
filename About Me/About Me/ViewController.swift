5b1ac6cc31a8a5158bd6b32709fe93ea6a44d426
Clantigua2 <clantigua954@gmail.com>
2019-05-09 21:58:29 -0400

    made button print out my name

---
+++ About Me/About Me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Space Ghost lantigua on 5/9/19.
+//  Copyright © 2019 Carlos Lantigua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var introduceButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func introduceButtonClicked(_ sender: Any) {
+        let name: String = "Carlos"
+        print(name)
+    }
+    
+}
+
